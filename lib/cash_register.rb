4a0984fe14f4480d222be81bb8c60f4b6bf0efbf
simonemayers <simone21295@gmail.com>
2020-10-28 02:51:25 -0400

    commit tree b8d38e5e9233f0f758ce697a29ab48333a7dc34d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
-
+class CashRegister
+    attr_accessor :total, :discount, :previous_total, :items
+    def initialize(discount = nil)
+        @total = 0
+        @discount = discount
+    end
+    def total
+        @total
+    end
+    def add_item(title, price, quantity = 1)
+        @previous_total = @total
+        @total = @total + (price * quantity)
+        @items = [] if @items == nil
+        quantity.times {@items << title}
+    end
+    def apply_discount
+        if @discount == nil 
+            "There is no discount to apply."
+        else
+            @total = @total - (@total * ((@discount.to_f) / 100))
+            "After the discount, the total comes to $#{total.to_i}."
+        end
+    end
+    def void_last_transaction
+        @total = @previous_total 
+    end
+end
